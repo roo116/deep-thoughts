6f8070e040e8621e38196cd4d03fe993d7b6a206
Chip Hill <hillkarl03@gmail.com>
2022-10-06 02:56:36 -0400

    created thought typedefs, resolvers, added reactions, and tested

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,17 @@
+const {User, Thought} = require('../models')
+
+
+
+
+
+// thoughts resolver - query thoughts; async function that returns a) Find the Thought, then sort in descending order when thought was created. 
+
+
 const resolvers = {
   Query: {
-    helloWorld: () => {
-      return "Hello world!";
+   thoughts: async (parent, {username}) => {
+    const params = username ? {username} : {};
+      return Thought.find(params).sort({createdAt: -1});
     },
   },
 };
